9b2287272c6ea6d5616c8d246fd8f4ce1daebb89
新家博文 <hiro@hiro-no-MacBook-puro.local>
2019-03-23 17:41:54 +0900

    xxxxx

--- epcltest.el
+++ epcltest.el
@@ -1,48 +1,99 @@
 
 
-(require 'epcl)
+;; (require 'epcl)
 (load "epcl")
-
-(defun test-parser (p str)
-  (with-temp-buffer
-    (insert str)
-    (goto-char (point-min))
-    (epcl-apply p)))
 
 (defvar abc nil)
 (defvar abc2 nil)
 
-(setq abc
-      (let* ((a (epcl-token (epcl-regexp "a")))
-	     (b (epcl-token (epcl-regexp "b")))
-	     (c (epcl-token (epcl-regexp "c")))
-	     (abc (epcl-seq a b c)))
-	abc))
-
-(setq abc2
-      (let* ((a (epcl-token (epcl-regexp "a")))
-	     (b (epcl-token (epcl-regexp "b")))
-	     (c (epcl-token (epcl-regexp "c")))
-	     (abc (epcl-or a b c)))
-	abc))
-
-(test-parser abc "   a  b c")
-(test-parser abc2 "   b")
+(ert-deftest basic-test ()
+  (should
+   (equal
+    (epcl--success 3 "a" "a")
+    (let* ((a (epcl-regexp "a"))
+	   (aa (epcl-seq a a)))
+      (epcl-parse-string aa "aa")
+      )
+    )
+   ))
 
 
-(let* ((a (epcl-token (epcl-regexp "[[:digit:]]+")))
-       (i (epcl-bind a #'string-to-number)))
-  (test-parser i "    10"))
+(ert-deftest token-test ()
+  (should (equal
+	   (let* ((a (epcl-token (epcl-regexp "[[:digit:]]+")))
+		  (i (epcl-bind a #'string-to-number)))
+	     (epcl-parse-string i "    10"))
+	   (epcl--success 7 10)))
+  (should (equal
+	   (let* ((a (epcl-token (epcl-regexp "[[:digit:]]+"))))
+	     (epcl-parse-string a "    10"))
+	   (epcl--success 7 "10")))
+  )  
 
-(let* ((a (epcl-token (epcl-regexp "[[:digit:]]+"))))
-  (test-parser a "    10"))
+(ert-deftest epcl-seq-test ()
+  (let* ((a (epcl-token (epcl-regexp "a")))
+	 (b (epcl-token (epcl-regexp "b")))
+	 (c (epcl-token (epcl-regexp "c")))
+	 (abc (epcl-seq a b c)))
+    (should (equal
+	     (epcl-parse-string abc "    a b  c")
+	     (epcl--success 11 "a" "b" "c")))
+    )
+  )
 
-(let* ((a (epcl-regexp "[[:digit:]]+")))
-    (test-parser a "10"))
+(ert-deftest epcl-or-test ()
 
-(cl-defstruct token x y z)
+  (let* ((a (epcl-token (epcl-regexp "a")))
+	 (b (epcl-token (epcl-regexp "b")))
+	 (c (epcl-token (epcl-regexp "c")))
+	 (abc (epcl-or (epcl-try a)
+		       (epcl-try b)
+		       c)))
+    (should (equal
+	     (epcl-parse-string abc "    b")
+	     (epcl--success 6 "b"))
+	    )
+    )
+  )
 
-(setq x (make-token :x 10 :y 20 :z 30))
+(ert-deftest epcl-let-test ()
+  (should
+   (equal
+    '(epcl-bind
+      (epcl-seq p1
+		(epcl-discard p2)
+		p3)
+      (lambda
+	(a b)
+	(list a b)))
+    (macroexpand-1
+     '(epcl-let ((a p1)
+		 p2
+		 (b p3)
+		 )
+		(list a b)))
+    )
+   )
+  )
 
-(token-x x)
-(token-y x)
+(ert-deftest epcl-let-apply-test ()
+  (should
+   (equal
+    (epcl--success 4 (list "a" "c"))
+    (let* ((p (epcl-regexp "a"))
+	   (q (epcl-regexp "b"))
+	   (r (epcl-regexp "c"))
+	   (x (epcl-let ((a p)
+			 q
+			 (b r)
+			 )
+			(list a b))))
+      (epcl-parse-string x "abc"))
+    )
+   )
+  )
+
+
+
+
+
